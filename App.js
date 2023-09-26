9a1f0f001d7f1664e6813c54063add731a44dcb1
kasimyuksel01 <kasimyuksel98@gmail.com>
2023-09-26 19:02:27 +0300

    uygulama npm kütüphanesi kullanarak çalışır halde

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { isValidCreditCard } from 'kredi-karti-validasyon';
 
 export default function App() {
+  const [cardNumber, setCardNumber] = useState('');
+  const [formattedCardNumber, setFormattedCardNumber] = useState('');
+  const [isValid, setIsValid] = useState(null);
+
+  const formatCardNumber = (input) => {
+    // Girilen numarayı yalnızca rakam olarak al, her 4 karakterde bir boşluk ekleyerek düzenle
+    const numbersOnly = input.replace(/\D/g, '');
+    return numbersOnly.replace(/(\d{4})/g, '$1 ').trim();
+  };
+
+  const validateCard = () => {
+    const cleanedCardNumber = cardNumber.replace(/\D/g, ''); // Girilen numaradan tüm harf ve boşlukları kaldır
+
+    if (/^[0-9]{16}$/.test(cleanedCardNumber)) {
+      const isValidCard = isValidCreditCard(cleanedCardNumber);
+      setIsValid(isValidCard);
+    } else {
+      setIsValid(false);
+    }
+  };
+
+  const handleCardNumberChange = (text) => {
+    const formatted = formatCardNumber(text);
+    setFormattedCardNumber(formatted);
+    setCardNumber(formatted.replace(/\s+/g, ''));
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Kredi Kartı Numarası (16 Hane):</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={handleCardNumberChange}
+        value={formattedCardNumber}
+        keyboardType="numeric"
+        maxLength={19} // 16 rakam ve 3 boşluk
+      />
+      <Button title="Doğrula" onPress={validateCard} />
+      {isValid !== null && (
+        <Text style={isValid ? styles.valid : styles.invalid}>
+          {isValid ? 'Geçerli' : 'Geçersiz'} kredi kartı
+        </Text>
+      )}
     </View>
   );
 }
@@ -13,8 +53,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'white'
+  },
+  input: {
+    width: 200,
+    borderBottomWidth: 1,
+    marginBottom: 20,
+  },
+  valid: {
+    color: 'green',
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  invalid: {
+    color: 'red',
+    fontWeight: 'bold',
+    marginTop: 10,
   },
 });
